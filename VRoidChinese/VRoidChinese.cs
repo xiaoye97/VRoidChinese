5bf5397c87b6a6775f0877f09214ee22cf7035e2
xiaoye97 <1066666683@qq.com>
2021-02-27 10:58:11 +0800

    更新插件 1.4
    大幅提高加载速度

--- VRoidChinese/VRoidChinese.cs
+++ VRoidChinese/VRoidChinese.cs
@@ -1,24 +1,28 @@
-﻿using System;
+using System;
 using BepInEx;
+using System.IO;
 using HarmonyLib;
 using UnityEngine;
 using System.Linq;
-using BepInEx.Configuration;
+using System.Text;
 using System.Reflection.Emit;
 using System.Collections.Generic;
 
 namespace VRoidChinese
 {
-    [BepInPlugin("VRoid.Chinese", "VRoid汉化插件", "1.3")]
+    [BepInPlugin("VRoid.Chinese", "VRoid汉化插件", "1.4")]
     public class VRoidChinese : BaseUnityPlugin
     {
         //中文配置
-        public static Dictionary<string, ConfigEntry<string>> zhDict = new Dictionary<string, ConfigEntry<string>>();
+        public static string zhPath;
+        private static bool needSave;
+        public static Dictionary<string, string> zhConfig = new Dictionary<string, string>();
         public static Harmony harmony;
         public static Type TranslatorType, WelcomeControllerType;
 
         void Start()
         {
+            zhPath = $"{Paths.ConfigPath}/VRoid.Chinese.cfg";
             //初始化
             TranslatorType = AccessTools.TypeByName("Translator");
             WelcomeControllerType = AccessTools.TypeByName("WelcomeController");
@@ -26,10 +30,18 @@ namespace VRoidChinese
             //与配置文件进行配对
             var enDict = Traverse.Create(TranslatorType).Field("enDictionary").GetValue<Dictionary<string, string>>();
             enDict.Add("GUI.Welcome.Preferences.Language.Chinese", "中文");
+            LoadConfig();
             foreach (var k in enDict.Keys)
             {
-                var config = Config.Bind<string>("翻译", k, enDict[k]);
-                zhDict.Add(k, config);
+                if (!zhConfig.ContainsKey(k))
+                {
+                    zhConfig.Add(k, enDict[k]);
+                    needSave = true;
+                }
+            }
+            if (needSave)
+            {
+                SaveConfig();
             }
             //添加中文选项
             harmony.Patch(WelcomeControllerType.GetProperty("LanguagePreferencesDropdownItemList").GetGetMethod(), prefix: new HarmonyMethod(typeof(VRoidChinese).GetMethod("WelcomeController_LanguagePreferencesDropdownItemList_Get_Prefix")));
@@ -37,19 +49,61 @@ namespace VRoidChinese
             harmony.Patch(AccessTools.Method(WelcomeControllerType, "UpdateLanguagePreferencesWithIndex"), transpiler: new HarmonyMethod(typeof(VRoidChinese).GetMethod("WelcomeController_UpdateLanguagePreferencesWithIndex_Transpiler")));
             //设置标题
             harmony.Patch(AccessTools.Method(AccessTools.TypeByName("WindowTitle"), "Update"), prefix: new HarmonyMethod(typeof(VRoidChinese).GetMethod("WindowTitle_Update_Prefix")));
-            //刷新数据
-            RefreshLanguage();
+            //添加语言数据
+            AddLanguageDictToVRoidStudio();
             //设定当前语言为中文
             Traverse.Create(GameObject.FindObjectOfType(WelcomeControllerType)).Method("OnLanguagePreferencesDropdownValueChanged", 2).GetValue();
         }
 
-        void Update()
+        /// <summary>
+        /// 加载配置
+        /// </summary>
+        void LoadConfig()
         {
-            if (Input.GetKeyDown(KeyCode.F5))
+            if (File.Exists(zhPath))
             {
-                Logger.LogInfo("刷新语言数据");
-                RefreshLanguage();
+                var lines = File.ReadAllLines(zhPath);
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    if (!string.IsNullOrWhiteSpace(lines[i]))
+                    {
+                        var strs = lines[i].Split('=');
+                        if (strs.Length == 2)
+                        {
+                            if (strs[1].Contains("\\n"))
+                            {
+                                string str = strs[1].Replace("\\n", "\n");
+                                zhConfig.Add(strs[0], str);
+                            }
+                            else
+                            {
+                                zhConfig.Add(strs[0], strs[1]);
+                            }
+                        }
+                    }
+                }
             }
+        }
+
+        /// <summary>
+        /// 保存配置
+        /// </summary>
+        void SaveConfig()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var kv in zhConfig)
+            {
+                if (kv.Value.Contains("\n"))
+                {
+                    string str = kv.Value.Replace("\n", "\\n");
+                    sb.AppendLine($"{kv.Key}={str}");
+                }
+                else
+                {
+                    sb.AppendLine($"{kv.Key}={kv.Value}");
+                }
+            }
+            File.WriteAllText($"{Paths.ConfigPath}/VRoidChinese.cfg", sb.ToString());
         }
 
         /// <summary>
@@ -77,35 +131,17 @@ namespace VRoidChinese
         }
 
         /// <summary>
-        /// 刷新语言数据
+        /// 添加语言数据
         /// </summary>
-        private void RefreshLanguage()
+        private void AddLanguageDictToVRoidStudio()
         {
             var dict = Traverse.Create(TranslatorType).Field("dictionaries").GetValue<Dictionary<string, Dictionary<string, string>>>();
-            Dictionary<string, string> editCache = new Dictionary<string, string>();
-            if (dict.ContainsKey("zh"))
+            Dictionary<string, string> zh = new Dictionary<string, string>();
+            foreach (var kv in zhConfig)
             {
-                foreach (var kv in dict["zh"])
-                {
-                    if (zhDict.ContainsKey(kv.Key) && kv.Value != zhDict[kv.Key].Value)
-                    {
-                        editCache.Add(kv.Key, zhDict[kv.Key].Value);
-                    }
-                }
-                foreach (var kv in editCache)
-                {
-                    dict["zh"][kv.Key] = kv.Value;
-                }
+                zh.Add(kv.Key, kv.Value);
             }
-            else
-            {
-                Dictionary<string, string> zh = new Dictionary<string, string>();
-                foreach (var kv in zhDict)
-                {
-                    zh.Add(kv.Key, kv.Value.Value);
-                }
-                dict.Add("zh", zh);
-            }
+            dict.Add("zh", zh);
         }
 
         /// <summary>
