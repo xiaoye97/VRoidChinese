5afccb517c1c4f5db242ab091c4849e63905aa2d
xiaoye97 <1163537847@qq.com>
2021-11-01 00:39:43 +0800

    适配VRoidStudio 1.0
    针对1.0版本重写了汉化插件
    删除了config文件夹，后续将使用新文件夹和新的配置文件

--- VRoidChinese/VRoidChinese.cs
+++ VRoidChinese/VRoidChinese.cs
@@ -7,149 +7,330 @@ using System.Linq;
 using System.Text;
 using System.Reflection.Emit;
 using System.Collections.Generic;
+using VRoid.UI.Messages;
+using Newtonsoft.Json;
+using System.Collections;
+using System.Reflection;
+using BepInEx.Configuration;
+using System.Globalization;
+using StandaloneWindowTitleChanger;
 
 namespace VRoidChinese
 {
-    [BepInPlugin("VRoid.Chinese", "VRoid汉化插件", "1.5")]
+    [BepInPlugin("VRoid.Chinese", "VRoid汉化插件", "1.6")]
     public class VRoidChinese : BaseUnityPlugin
     {
-        //中文配置
-        public static string zhPath;
-        private static bool needSave;
-        public static Dictionary<string, string> zhConfig = new Dictionary<string, string>();
-        public static Harmony harmony;
-        public static Type TranslatorType, WelcomeControllerType;
+        public DirectoryInfo WorkDir = new DirectoryInfo($"{Paths.GameRootPath}/Chinese");
+        public ConfigEntry<bool> OnStartDump;
+        public ConfigEntry<bool> OnFallbackDump;
+        public ConfigEntry<bool> DevMode;
+        public ConfigEntry<KeyCode> RefreshLangKey;
 
-        void Start()
+        public static List<string> MessagesNullItems = new List<string>();
+
+        /// <summary>
+        /// 是否有空值，有空值则需要Dump
+        /// </summary>
+        public static bool HasNullValue;
+
+        public string ENMessage;
+
+        public static bool ShowUpdateTip;
+        // 是否进行了回退
+        public static bool IsFallback;
+        public Vector2 tipV2;
+
+        private void Start()
         {
-            zhPath = $"{Paths.ConfigPath}/VRoid.Chinese.cfg";
-            //初始化
-            TranslatorType = AccessTools.TypeByName("Translator");
-            WelcomeControllerType = AccessTools.TypeByName("WelcomeController");
-            harmony = new Harmony("VRoid.Chinese");
-            //与配置文件进行配对
-            var enDict = Traverse.Create(TranslatorType).Field("enDictionary").GetValue<Dictionary<string, string>>();
-            enDict.Add("GUI.Welcome.Preferences.Language.Chinese", "中文");
-            LoadConfig();
-            foreach (var k in enDict.Keys)
+            if (!WorkDir.Exists)
             {
-                if (!zhConfig.ContainsKey(k))
+                WorkDir.Create();
+            }
+            ENMessage = JsonConvert.SerializeObject(Messages.All["en"]);
+            OnStartDump = Config.Bind<bool>("config", "OnStartDump", false, "当启动时进行转储");
+            OnFallbackDump = Config.Bind<bool>("config", "OnFallbackDump", false, "当触发fallback时进行转储");
+            DevMode = Config.Bind<bool>("config", "DevMode", false, "汉化者开发模式");
+            RefreshLangKey = Config.Bind<KeyCode>("config", "RefreshLangKey", KeyCode.F10, "[仅限开发模式]刷新语言快捷键");
+            if (OnStartDump.Value)
+            {
+                Dump();
+            }
+            StartToChinese();
+            Harmony.CreateAndPatchAll(typeof(VRoidChinese));
+            StandaloneWindowTitle.Change("VRoid Studio");
+        }
+
+        private void Update()
+        {
+            if (DevMode.Value)
+            {
+                if (Input.GetKeyDown(RefreshLangKey.Value))
                 {
-                    zhConfig.Add(k, enDict[k]);
-                    needSave = true;
+                    StartToChinese();
                 }
             }
-            if (needSave)
+        }
+
+        private void OnGUI()
+        {
+            if (ShowUpdateTip)
             {
-                SaveConfig();
+                Rect rect = new Rect(Screen.width / 2 - 200, Screen.height / 2 - 100, 400, 200);
+                rect = GUILayout.Window(1234, rect, TipWindowFunc, "有缺失的汉化", GUILayout.ExpandHeight(true));
             }
-            //添加中文选项
-            harmony.Patch(WelcomeControllerType.GetProperty("LanguagePreferencesDropdownItemList").GetGetMethod(), prefix: new HarmonyMethod(typeof(VRoidChinese).GetMethod("WelcomeController_LanguagePreferencesDropdownItemList_Get_Prefix")));
-            //修正语言选项刷新索引上限
-            harmony.Patch(AccessTools.Method(WelcomeControllerType, "UpdateLanguagePreferencesWithIndex"), transpiler: new HarmonyMethod(typeof(VRoidChinese).GetMethod("WelcomeController_UpdateLanguagePreferencesWithIndex_Transpiler")));
-            //设置标题
-            harmony.Patch(AccessTools.Method(AccessTools.TypeByName("WindowTitle"), "Update"), prefix: new HarmonyMethod(typeof(VRoidChinese).GetMethod("WindowTitle_Update_Prefix")));
-            //添加语言数据
-            AddLanguageDictToVRoidStudio();
-            //设定当前语言为中文
-            Traverse.Create(GameObject.FindObjectOfType(WelcomeControllerType)).Method("OnLanguagePreferencesDropdownValueChanged", 2).GetValue();
+        }
+
+        public void TipWindowFunc(int id)
+        {
+            GUI.backgroundColor = Color.white;
+            GUI.contentColor = Color.black;
+            GUILayout.Label("检查到有缺失的词条，可以前往Github查看汉化是否有更新。");
+            GUILayout.Label("如果Github上未更新汉化，可以到VRoid交流群找我反馈。");
+            GUILayout.Label("汉化作者:xiaoye97");
+            GUILayout.Label("QQ:1066666683");
+            GUILayout.Label("B站:宵夜97");
+            GUILayout.Label("VRoid交流群:684544577");
+            GUILayout.Label(" ");
+            if (IsFallback)
+            {
+                GUI.contentColor = Color.red;
+                GUILayout.Label("由于缺失词条导致异常，已回退到英文。");
+            }
+            GUI.contentColor = Color.black;
+            if (GUILayout.Button("确定"))
+            {
+                ShowUpdateTip = false;
+            }
         }
 
         /// <summary>
-        /// 加载配置
+        /// 转储词条
         /// </summary>
-        void LoadConfig()
+        public void Dump()
         {
-            if (File.Exists(zhPath))
+            Debug.Log("开始Dump Messages...");
+            var en = JsonConvert.SerializeObject(Messages.All["en"], Formatting.Indented);
+            File.WriteAllText($"{WorkDir.FullName}/DumpMessages_en.json", en);
+            Debug.Log("开始Dump String...");
+            var s_localeStringDictionary = Traverse.Create(typeof(Messages)).Field("s_localeStringDictionary").GetValue<Dictionary<string, Dictionary<string, string>>>();
+            var enDict = s_localeStringDictionary["en"];
+            StringBuilder sb = new StringBuilder();
+            foreach (var kv in enDict)
             {
-                var lines = File.ReadAllLines(zhPath);
-                for (int i = 0; i < lines.Length; i++)
+                string value = kv.Value.Replace("\r\n", "\\r\\n");
+                sb.AppendLine($"{kv.Key}={value}");
+            }
+            File.WriteAllText($"{WorkDir.FullName}/DumpString_en.txt", sb.ToString());
+        }
+
+        /// <summary>
+        /// 开始汉化
+        /// </summary>
+        public void StartToChinese()
+        {
+            MessagesNullItems.Clear();
+            HasNullValue = false;
+            Logger.LogInfo("开始汉化...");
+            FixMessages();
+            FixString();
+            Logger.LogInfo("刷新界面...");
+            try
+            {
+                Messages.OnMessagesLanguageChange();
+            }
+            catch (Exception e)
+            {
+                Logger.LogError($"刷新界面出现异常:{e.Message}\n{e.StackTrace}");
+                Logger.LogInfo("回退汉化...");
+                IsFallback = true;
+                var ori = Traverse.Create(typeof(Messages)).Field("s_localeDictionary").GetValue<Dictionary<string, Messages>>();
+                ori["en"] = JsonConvert.DeserializeObject<Messages>(ENMessage);
+                Traverse.Create(typeof(Messages)).Field("s_localeDictionary").SetValue(ori);
+                Messages.OnMessagesLanguageChange();
+            }
+        }
+
+        /// <summary>
+        /// 汉化Messages
+        /// </summary>
+        public void FixMessages()
+        {
+            Logger.LogInfo("开始汉化Messages...");
+            if (File.Exists($"{WorkDir}/MessagesChinese.json"))
+            {
+                Logger.LogInfo("检测到Messages汉化文件,开始读取文件...");
+                string json;
+                try
                 {
-                    if (!string.IsNullOrWhiteSpace(lines[i]))
+                    json = File.ReadAllText($"{WorkDir}/MessagesChinese.json");
+                }
+                catch (Exception e)
+                {
+                    Logger.LogError($"读取Messages汉化文件出现异常:{e.Message}\n{e.StackTrace}");
+                    return;
+                }
+                Logger.LogInfo("开始解析Messages汉化文件...");
+                Messages cn;
+                try
+                {
+                    cn = JsonConvert.DeserializeObject<Messages>(json);
+                    File.WriteAllText($"{WorkDir}/test1.json", JsonConvert.SerializeObject(cn, Formatting.Indented));
+                }
+                catch (Exception e)
+                {
+                    Logger.LogError($"解析Messages汉化文件出现异常:{e.Message}\n{e.StackTrace}");
+                    return;
+                }
+                Logger.LogInfo("检查缺失的词条...");
+                Messages cnCheck;
+                try
+                {
+                    cnCheck = FallbackCopy(cn, Messages.All["en"]);
+                }
+                catch (Exception e)
+                {
+                    Logger.LogError($"检查缺失的词条出现异常:{e.Message}\n{e.StackTrace}");
+                    return;
+                }
+                if (HasNullValue)
+                {
+                    Logger.LogWarning("触发fallback,需要通知汉化作者进行更新.");
+                    if (OnFallbackDump.Value)
                     {
-                        var strs = lines[i].Split('=');
-                        if (strs.Length == 2)
+                        Dump();
+                    }
+                }
+                Logger.LogInfo("开始将中文Messages对象替换到英文对象...");
+                try
+                {
+                    var ori = Traverse.Create(typeof(Messages)).Field("s_localeDictionary").GetValue<Dictionary<string, Messages>>();
+                    ori["en"] = cn;
+                    Traverse.Create(typeof(Messages)).Field("s_localeDictionary").SetValue(ori);
+                }
+                catch (Exception e)
+                {
+                    Logger.LogError($"将中文Messages对象替换到英文对象出现异常:{e.Message}\n{e.StackTrace}");
+                    return;
+                }
+                Logger.LogInfo("Messages汉化完毕.");
+            }
+            else
+            {
+                Logger.LogError($"未检测到Messages汉化文件{WorkDir}/MessagesChinese.json,请检查安装.");
+            }
+        }
+
+        /// <summary>
+        /// 带退路的拷贝(对VRoidStudio的汉化无用，VRoidStudio使用只读属性，这里只用来检查空值)
+        /// </summary>
+        public static T FallbackCopy<T>(T target, T fallback)
+        {
+            // 检查是否为空，为空则使用后备
+            if (target == null)
+            {
+                HasNullValue = true;
+                ShowUpdateTip = true;
+                Debug.LogWarning($"Messages缺失汉化:{fallback}");
+                if (target is string)
+                {
+                    MessagesNullItems.Add(fallback as string);
+                }
+                return fallback;
+            }
+            // 如果是string或者值类型则直接返回
+            if (target is string || target.GetType().IsValueType)
+            {
+                return target;
+            }
+
+            object retval;
+            try
+            {
+                retval = Activator.CreateInstance(target.GetType());
+            }
+            catch
+            {
+                var json = JsonConvert.SerializeObject(fallback);
+                retval = JsonConvert.DeserializeObject<T>(json);
+            }
+            PropertyInfo[] pros = target.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            foreach (var pro in pros)
+            {
+                try
+                {
+                    object pt = pro.GetValue(target);
+                    object pf = pro.GetValue(fallback);
+                    object o = FallbackCopy(pt, pf);
+                    pro.SetValue(retval, o);
+                }
+                catch { }
+            }
+            return (T)retval;
+        }
+
+        /// <summary>
+        /// 汉化常规文本
+        /// </summary>
+        public void FixString()
+        {
+            Logger.LogInfo("开始汉化常规文本...");
+            if (File.Exists($"{WorkDir}/StringChinese.txt"))
+            {
+                Logger.LogInfo("检测到String汉化文件,开始读取文件...");
+                string[] lines;
+                try
+                {
+                    lines = File.ReadAllLines($"{WorkDir}/StringChinese.txt");
+                }
+                catch (Exception e)
+                {
+                    Logger.LogError($"读取String汉化文件出现异常:{e.Message}\n{e.StackTrace}");
+                    return;
+                }
+                Logger.LogInfo("开始解析String汉化文件...");
+                var s_localeStringDictionary = Traverse.Create(typeof(Messages)).Field("s_localeStringDictionary").GetValue<Dictionary<string, Dictionary<string, string>>>();
+                var strDict = s_localeStringDictionary["en"];
+                try
+                {
+                    foreach (var line in lines)
+                    {
+                        if (!string.IsNullOrWhiteSpace(line))
                         {
-                            if (strs[1].Contains("\\n"))
+                            var kv = line.Split(new char[] { '=' }, 2);
+                            if (kv.Length == 2)
                             {
-                                string str = strs[1].Replace("\\n", "\n");
-                                zhConfig.Add(strs[0], str);
-                            }
-                            else
-                            {
-                                zhConfig.Add(strs[0], strs[1]);
+                                strDict[kv[0]] = kv[1].Replace("\\r\\n", "\r\n");
                             }
                         }
                     }
                 }
-            }
-        }
-
-        /// <summary>
-        /// 保存配置
-        /// </summary>
-        void SaveConfig()
-        {
-            StringBuilder sb = new StringBuilder();
-            foreach (var kv in zhConfig)
-            {
-                if (kv.Value.Contains("\n"))
+                catch (Exception e)
                 {
-                    string str = kv.Value.Replace("\n", "\\n");
-                    sb.AppendLine($"{kv.Key}={str}");
+                    Logger.LogError($"解析String汉化文件出现异常:{e.Message}\n{e.StackTrace}");
+                    return;
                 }
-                else
+                Logger.LogInfo("开始将中文String替换到英文...");
+                try
                 {
-                    sb.AppendLine($"{kv.Key}={kv.Value}");
+                    Traverse.Create(typeof(Messages)).Field("s_localeStringDictionary").SetValue(s_localeStringDictionary);
                 }
+                catch (Exception e)
+                {
+                    Logger.LogError($"将中文String替换到英文出现异常:{e.Message}\n{e.StackTrace}");
+                    return;
+                }
+                Logger.LogInfo("String汉化完毕.");
             }
-            File.WriteAllText($"{Paths.ConfigPath}/VRoid.Chinese.cfg", sb.ToString());
-        }
-
-        /// <summary>
-        /// 添加中文选项补丁
-        /// </summary>
-        public static bool WelcomeController_LanguagePreferencesDropdownItemList_Get_Prefix(ref KeyValuePair<string, string>[] __result)
-        {
-            __result = new KeyValuePair<string, string>[]
+            else
             {
-                new KeyValuePair<string, string>("en", "GUI.Welcome.Preferences.Language.English"),
-                new KeyValuePair<string, string>("ja", "GUI.Welcome.Preferences.Language.Japanese"),
-                new KeyValuePair<string, string>("zh", "GUI.Welcome.Preferences.Language.Chinese")
-            };
-            return false;
-        }
-
-        /// <summary>
-        /// 中文选项刷新索引补丁
-        /// </summary>
-        public static IEnumerable<CodeInstruction> WelcomeController_UpdateLanguagePreferencesWithIndex_Transpiler(IEnumerable<CodeInstruction> instructions)
-        {
-            List<CodeInstruction> codes = new List<CodeInstruction>(instructions);
-            codes[8].opcode = OpCodes.Ldc_I4_3;
-            return codes.AsEnumerable();
-        }
-
-        /// <summary>
-        /// 添加语言数据
-        /// </summary>
-        private void AddLanguageDictToVRoidStudio()
-        {
-            var dict = Traverse.Create(TranslatorType).Field("dictionaries").GetValue<Dictionary<string, Dictionary<string, string>>>();
-            Dictionary<string, string> zh = new Dictionary<string, string>();
-            foreach (var kv in zhConfig)
-            {
-                zh.Add(kv.Key, kv.Value);
+                Logger.LogError($"未检测到String汉化文件{WorkDir}/StringChinese.txt,请检查安装.");
             }
-            dict.Add("zh", zh);
         }
 
-        /// <summary>
-        /// 修改标题
-        /// </summary>
-        public static bool WindowTitle_Update_Prefix(ref string title)
+        [HarmonyPrefix, HarmonyPatch(typeof(StandaloneWindowTitle), "Change")]
+        public static bool WindowTitlePatch(ref string newTitle)
         {
-            title += " 中文汉化 By xiaoye97";
+            newTitle += " 中文汉化 By 宵夜97";
             return true;
         }
     }
